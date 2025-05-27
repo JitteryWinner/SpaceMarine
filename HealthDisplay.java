24b069c720b6313336b3e7e0683878b342b2f052
JitteryWinner <davidaberganza@gmail.com>
2025-05-26 18:26:52 -0600

    Delete HealthDisplay.java

--- HealthDisplay.java
+++
@@ -1,9 +0,0 @@
-import greenfoot.*;
-
-public class HealthDisplay extends Actor {
-    public HealthDisplay() {
-        setImage(new GreenfootImage(20, 20));
-        getImage().setColor(Color.GREEN);
-        getImage().fillOval(0, 0, 20, 20);
-    }
-}
